aa3e60d92ab5965eff6fede3c54402dbd046061e
MartinTarigan <martin.marcelino@ui.ac.id>
2024-02-22 22:18:03 +0700

    feat(navbar): implement responsive navigation bar

---
+++ lib/core/widgets/navbar.dart
@@ -0,0 +1,311 @@
+import 'package:flutter/material.dart';
+import 'package:questlist/app.dart';
+import 'package:questlist/core/theme/base_color.dart';
+
+class PersistentBottomNavPage extends StatelessWidget {
+  final _tab1navigatorKey = GlobalKey<NavigatorState>();
+  final _tab2navigatorKey = GlobalKey<NavigatorState>();
+  final _tab3navigatorKey = GlobalKey<NavigatorState>();
+
+  PersistentBottomNavPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PersistentBottomBarScaffold(
+      items: [
+        PersistentTabItem(
+          tab: const ToDoListPage(),
+          icon: Icons.home_rounded,
+          title: 'Home',
+          navigatorkey: _tab1navigatorKey,
+        ),
+        PersistentTabItem(
+          tab: const ToDoListPage(),
+          icon: Icons.shopping_cart_rounded,
+          title: 'Order',
+          navigatorkey: _tab2navigatorKey,
+        ),
+        PersistentTabItem(
+          tab: const ToDoListPage(),
+          icon: Icons.person,
+          title: 'Profile',
+          navigatorkey: _tab3navigatorKey,
+        ),
+      ],
+    );
+  }
+}
+
+class TabPage1 extends StatelessWidget {
+  const TabPage1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tab 1')),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Tab 1'),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const Page1('Tab1')));
+                },
+                child: const Text('Go to page1'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TabPage2 extends StatelessWidget {
+  const TabPage2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tab 2')),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Tab 2'),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const Page2('tab2')));
+                },
+                child: const Text('Go to page2'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TabPage3 extends StatelessWidget {
+  const TabPage3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tab 3')),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Tab 3'),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const Page3('tab3')));
+                },
+                child: const Text('Go to page2'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  final String inTab;
+
+  const Page1(this.inTab, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 1')),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('in $inTab Page 1'),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => Page2(inTab)));
+                },
+                child: const Text('Go to page2'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  final String inTab;
+
+  const Page2(this.inTab, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 2')),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('in $inTab Page 2'),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => Page3(inTab)));
+                },
+                child: const Text('Go to page3'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  final String inTab;
+
+  const Page3(this.inTab, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 3')),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('in $inTab Page 3'),
+            ElevatedButton(
+                onPressed: () {
+                },
+                child: const Text('Go back'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PersistentBottomBarScaffold extends StatefulWidget {
+  final List<PersistentTabItem> items;
+
+  const PersistentBottomBarScaffold({
+    Key? key,
+    required this.items,
+  }) : super(key: key);
+
+  @override
+  State<PersistentBottomBarScaffold> createState() =>
+      _PersistentBottomBarScaffoldState();
+}
+
+class _PersistentBottomBarScaffoldState
+    extends State<PersistentBottomBarScaffold> {
+  int _selectedTab = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double bottomPadding = MediaQuery.of(context).padding.bottom;
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedTab = index;
+          });
+        },
+        children: const [
+          ToDoListPage()
+        ],
+      ),
+      extendBody: true,
+      bottomNavigationBar: Stack(
+        clipBehavior: Clip.none,
+        alignment: const Alignment(0, 1),
+        children: [
+          _buildBottomNavigationBar(bottomPadding),
+          Positioned(
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: Center(child: FAB()),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomNavigationBar(double bottomPadding) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.symmetric(horizontal: 80, vertical: 40),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildNavItem(Icons.home_rounded, 'Home', 0),
+          _buildNavItem(Icons.person, 'Profile', 2),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String title, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedTab = index;
+          _pageController.jumpToPage(index);
+        });
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(icon,
+              color: _selectedTab == index ? BaseColors.purple : Colors.grey),
+          Text(title,
+              style: TextStyle(
+                  color:
+                      _selectedTab == index ? BaseColors.purple : Colors.grey)),
+        ],
+      ),
+    );
+  }
+}
+
+class PersistentTabItem {
+  final Widget tab;
+  final GlobalKey<NavigatorState>? navigatorkey;
+  final String title;
+  final IconData icon;
+
+  PersistentTabItem(
+      {required this.tab,
+      this.navigatorkey,
+      required this.title,
+      required this.icon});
+}
