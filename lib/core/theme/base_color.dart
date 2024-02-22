1cf1c462d8c90553c1f930e19037b93ef6255df1
MartinTarigan <martin.marcelino@ui.ac.id>
2024-02-22 22:13:51 +0700

    feat(core): add new base color theme for UI

---
+++ lib/core/theme/base_color.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class BaseColors {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFEFFFE);
+  static const Color purple = Color(0xFF6857e8);
+  static const Color primaryBlue = Color(0xFF65CFF2);
+  static const Color primaryGrey = Color(0xFFF5F4F4);
+  static const Color neutral = Color(0xFFF1F2F6);
+}
